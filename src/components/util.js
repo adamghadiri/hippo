4c54623a0ab6a656b6766924e71f14704886e1d7
Adam Ghadiri <adam.ghadiri@cenx.com>
2019-05-10 13:41:51 -0400

    better formatTime

--- src/components/util.js
+++ src/components/util.js
@@ -1,20 +1,25 @@
 import moment from "moment";
 
-function secondsToHm(d) {
-  d = Number(d);
-  var h = Math.floor(d / 3600);
-  var m = Math.floor((d % 3600) / 60);
+function secondsToHm(seconds) {
+  seconds = Number(seconds);
+  var d = Math.floor(seconds / (3600 * 24));
+  var h = Math.floor(seconds / 3600);
+  var m = Math.floor((seconds % 3600) / 60);
 
+  var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
   var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
   var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
-  return hDisplay + mDisplay;
+  return dDisplay + hDisplay + mDisplay;
 }
 
 const formatTime = ts => {
   const deltaSeconds = Math.floor(moment().valueOf() / 1000) - ts;
-  return deltaSeconds < 24 * 3600
-    ? `${secondsToHm(deltaSeconds)} ago`
-    : moment.unix(ts).format("HH:mm:ss YYYY/MM/DD");
+  if (deltaSeconds < 60) {
+    return `just now`;
+  } else if (deltaSeconds < 24 * 3600 * 30) {
+    return `${secondsToHm(deltaSeconds)}ago`;
+  }
+  return moment.unix(ts).format("HH:mm:ss YYYY/MM/DD");
 };
 
 export { formatTime };
